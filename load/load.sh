ba74ee8c2621904f7b003733be4f64b7c745b460
Andrew Schreiber <schreiaj@gmail.com>
2017-08-19 02:09:19 -0400

    added team data and split up match metadata

--- load/load.sh
+++ load/load.sh
@@ -1,3 +1,12 @@
+cat config_postgis.sql
+cat schema.sql
+
+# find ../data/events -type f -name "*_matches.csv" -exec cat {} \;
+# MIGHT end up being a less bad way to do it. Needs research
 find ../data/events -type f -name "*_matches.csv" | while read line; do
-  echo "\\\copy matches FROM $line WITH (FORMAT csv);"
+  echo "\\\copy raw_matches FROM $line WITH (FORMAT csv);"
 done
+
+echo "\\\copy raw_teams from ../local_data/all_teams.csv with (FORMAT CSV, HEADER true)"
+echo "REFRESH MATERIALIZED VIEW teams;"
+echo "REFRESH MATERIALIZED VIEW matches;"
--- load/schema.sql
+++ load/schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE matches;
+DROP TABLE raw_matches;
 
-CREATE TABLE matches (
+CREATE TABLE raw_matches (
   matchId varchar,
   red1 varchar,
   red2 varchar,
@@ -11,3 +11,85 @@ CREATE TABLE matches (
   redScore int,
   blueScore int
 );
+
+CREATE MATERIALIZED VIEW matches AS (
+  SELECT
+    matchid,
+    metadata[1] AS year,
+    metadata[2] AS event,
+    metadata[3] AS level,
+    metadata[4]  AS match,
+    array[red1, red2, red3] as redTeams,
+    array[blue1, blue2, blue3] as blueTeams,
+    array[red1, red2, red3, blue1, blue2, blue3] as allTeams,
+    CASE WHEN (redScore > blueScore) THEN array[red1, red2, red3]
+      WHEN (blueScore > redScore) THEN array[blue1, blue2, blue3]
+      ELSE null
+    END
+    as winner
+  FROM (
+    SELECT matchid,red1, red2, red3, blue1, blue2, blue3, redScore, blueScore, regexp_matches(matchid, '([0-9]{4})(.*?)_(.*)(m[0-9]+)') AS metadata
+    FROM raw_matches) AS data
+);
+
+
+DROP TABLE raw_teams;
+
+CREATE TABLE raw_teams (
+  district varchar,
+  locale varchar,
+  location varchar,
+  number varchar,
+  related varchar,
+  mostRecentYear varchar,
+  rookieYear varchar,
+  pos varchar,
+  accuratePos boolean,
+  schoolName varchar,
+  sponsors varchar,
+  notes varchar,
+  x varchar
+);
+
+-- Create MATERIALIZED VIEW to make it look like a table, far easier to refresh though
+
+CREATE MATERIALIZED VIEW teams AS
+  SELECT
+    CONCAT('frc', number) AS teamId,
+    number,
+    string_to_array(related, ',') AS related,
+    mostRecentYear,
+    rookieYear,
+    CASE WHEN pos SIMILAR TO '(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)' THEN st_pointfromtext(concat('POINT(',replace(pos, ',', ''),')'))
+      ELSE null
+    END AS location,
+    accuratePos,
+    schoolName,
+    sponsors,
+    CASE WHEN district = '#N/A' THEN null ELSE district END AS district
+  FROM raw_teams
+  WHERE number IS NOT NULL
+;
+
+
+-- Now into win/loss data
+
+CREATE VIEW most_wins AS
+  WITH teams AS (
+    SELECT unnest(winner) AS team
+    FROM matches
+  )
+  SELECT count(1) AS wins, team
+  FROM teams
+  GROUP BY team
+  ORDER BY wins DESC;
+
+CREATE VIEW most_plays AS
+  WITH teams AS (
+    SELECT unnest(allTeams) AS team
+    FROM matches
+  )
+  SELECT count(1) AS plays, team
+  FROM teams
+  GROUP BY team
+  ORDER BY plays DESC;
