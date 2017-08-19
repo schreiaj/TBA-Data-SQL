d861dd0c4fa6a805f005e1183d052fc6868efb8c
Andrew Schreiber <schreiaj@gmail.com>
2017-08-18 21:29:29 -0400

    change limit to 25 and add loosingest

--- load/useful_queries.sql
+++ load/useful_queries.sql
@@ -10,10 +10,24 @@ JOIN most_wins AS wins
   ON wins.team = plays.team
 WHERE plays.plays > 30
 ORDER BY win_pct DESC
-LIMIT 50;
+LIMIT 25;
 
 -- Compute "winningest" ie who has won the most matches
 SELECT team, wins
 FROM most_wins
 ORDER BY wins DESC
-LIMIT 50;
+LIMIT 25;
+
+-- Compute "loosingest" team. ie who has won the fewest
+-- Caveat - in > 30 matches
+SELECT
+  plays.team,
+  CAST(wins.wins AS FLOAT)/plays.plays*100.0 as win_pct,
+  plays.plays as matches,
+  wins.wins as match_wins
+FROM most_plays AS plays
+JOIN most_wins AS wins
+  ON wins.team = plays.team
+WHERE plays.plays > 30
+ORDER BY win_pct
+LIMIT 25;
